1de5d93c8c0c7b870e90a2ea0ca857e472e4a797
claubatista <contatodaclau@gmail.com>
2018-12-06 13:40:42 -0200

    add reset button functionality

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
-const playTimer = document.querySelectorAll('.btn__start');
-const timer = document.querySelectorAll('.timer', '.both__timer');
+const playTimer = document.querySelectorAll('.btn__start')
+const timer = document.querySelectorAll('.timer')
+const buttonReset = document.querySelectorAll('.btn__reset')
+
 
 let stopwatch = [playTimer.length]
 let timers = [0, 0, 0, 0, 0, 0, 0, 0]
 
-for (let i = 0; i < playTimer.length; i++)
-playTimer[i].addEventListener('click', function() {
-    playTimer[i].classList.toggle('btn__stop')
-    playTimer[i].classList.toggle('btn__start')
+for (let i = 0; i < playTimer.length; i++) {
+    playTimer[i].addEventListener('click', function() {
+        playTimer[i].classList.toggle('btn__stop')
+        playTimer[i].classList.toggle('btn__start')
 
-
-if (playTimer[i].classList.contains('btn__stop')){
+    if (playTimer[i].classList.contains('btn__stop')){
         this.innerHTML = 'STOP'
 
         stopwatch[i] = setInterval(function () {
-            timers[i]++;
-
-            timer[i].innerHTML = Math.floor(timers[i] / 60) + ':' + (timers[i]%60 <= 9 ? '0' + timers[i]%60 : timers[i]%60);
+            timers[i]++
+            const time = Math.floor(timers[i] / 60) + ':' + (timers[i]%60 <= 9 ? '0' + timers[i]%60 : timers[i]%60)
+            timer[i].innerHTML = time.padStart(5, '0')
         }, 1000)
+    }else {
+        this.innerHTML = 'START'
+        clearInterval(stopwatch[i]) 
     }
-    else {
-       this.innerHTML = 'START'
-       clearInterval(stopwatch[i]) 
-    }
-})
+    })
+
+    buttonReset[i].addEventListener('click', function() {
+        if (playTimer[i].classList.contains('btn__stop')){
+            playTimer[i].innerHTML = 'START'
+            playTimer[i].classList.toggle('btn__start')
+            playTimer[i].classList.toggle('btn__stop')
+            clearInterval(stopwatch[i])
+        }
+        timers[i] = 0
+        timer[i].innerHTML = '00:00'
+    })
+}
+   
+
  
 
 
